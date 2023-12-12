49ebc57241cd0fe535d8d6e0f581d388b5d63fa8
Deepak <deepakkumarkasyap12@gmail.com>
2023-12-12 18:49:10 +0530

    done

--- app/eduCard/page.jsx
+++ app/eduCard/page.jsx
@@ -1,48 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
-import wool2 from '../images/thread.jpeg'
-import { PiDotsThreeCircleVerticalDuotone } from "react-icons/pi";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 import { FaRegBookmark } from "react-icons/fa6";
 
 const page = () => {
-  return (
-    <div className='flex flex-col border-gray-400 border-2 w-2/4 '>
-        <div className='flex justify-between mt-2'>
-            <div className='flex ml-3'>
-                <div className='rounded-full'>
-                <Image src={wool2}  alt="....."width={40} height={40} className='border-black border-2 rounded-full'/>
+    return (
+        <div className='flex flex-col shadow-lg gap-2 p-4 w-[420px] font-poppins text-sm'>
+
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-2'>
+                    <img src='/img/avatar.png' alt="....." className='w-8 h-8 border-zinc-900 border-2 rounded-full' />
+                    <div className='flex flex-col text-xs'>
+                        <div className='font-semibold'>Ministry of Textile</div>
+                        <div className='text-gray-400 text-xs'>Aug 9</div>
+                    </div>
                 </div>
-                <div className='ml-3 font-semibold text-sm'>
-                    Ministry of Textile
+                <div className='flex items-center gap-2'>
+                    <div className='w-6 h-6 flex items-center justify-center border border-zinc-900 rounded-full cursor-pointer hover:bg-zinc-200'><FaRegBookmark /></div>
+                    <div className='w-6 h-6 flex items-center justify-center border border-zinc-900 rounded-full cursor-pointer hover:bg-zinc-200'><BiDotsVerticalRounded /></div>
                 </div>
             </div>
-            <div className='text-gray-400 text-sm mr-3'>
-                Aug 9
-            </div>
-        </div>
 
-        <div className='ml-3 font-bold mt-2 text-lg'>Improving Wool Farming</div>
-
-        <div className='flex m-3 text-justify'>
-            <div className='font-medium'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam non tempore accusantium totam deserunt incidunt recusandae sunt beatae.</div>
-            <div  className='w-5/8'>
-            <Image src={wool2}  alt="....."width={600} height={600} className='rounded-xl ml-2'/>
-            </div>
-        </div>
-
-        <div className='flex justify-between ml-3'>
-            <div className='text-gray-400 text-sm font-medium'>12 min read</div>
-            <div className='flex px-4 py-2'>
-                <div className='px-2 '>
-                <FaRegBookmark size={25}/>
-                </div>
-                <div className='border-black'>
-                <PiDotsThreeCircleVerticalDuotone size={25}/>
+            <div className='flex flex-col gap-1'>
+                <div className='font-bold text-lg'>Improving Wool Farming</div>
+                <div className='flex items-center gap-2'>
+                    <div className='font-medium text-xs'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam non tempore accusantium totam deserunt incidunt recusandae sunt beatae.</div>
+                    <img src='/img/wool.jpg' alt="....." className='rounded-lg w-[140px]' />
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default page
