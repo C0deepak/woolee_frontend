661f8b08240fbbbac142240d22d234ef4fbdbf34
Deepak <deepakkumarkasyap12@gmail.com>
2023-12-12 14:02:20 +0530

    font added

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import logo from '../images/sheep.jpeg'
 const page = () => {
   return (
-    <div>
+    <div className='font-poppins'>
       <div className='flex justify-center'>
         <Image src={logo}  alt="....."width={90} height={90} className='mt-4'/>
       </div>
       <div className='flex h-screen items-center flex-col'>
       <div className='shadow-xl px-5 py-5'>
         <div className='mb-2'>
-        <h1 className='font-bold text-2xl font-poppins'>Sign up</h1>
+        <h1 className='font-bold text-2xl'>Sign up</h1>
         <h3 className='text-gray-600 text-sm'>Create a new account</h3>
         </div>
         <form action="">
