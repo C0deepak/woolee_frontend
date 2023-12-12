e1867b4e5d0eb0825fc428686d1b810e23f717f8
Deepak <deepakkumarkasyap12@gmail.com>
2023-12-12 17:04:40 +0530

    new changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import Navbar from '@/components/Navbar/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      Woolee
+      <Navbar />
     </div>
   )
 }
