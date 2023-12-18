a97ed33601e0acd957d898fc8f596bedf6107b85
Deepak <deepakkumarkasyap12@gmail.com>
2023-12-18 21:31:32 +0530

    d changes

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,33 @@
 'use client'
 import { useAuth } from '@/context/authContext'
-import React from 'react'
-import { GiMoneyStack, GiPayMoney, GiReceiveMoney, GiTakeMyMoney } from 'react-icons/gi'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { GiMoneyStack, GiReceiveMoney, GiSheep, GiTakeMyMoney } from 'react-icons/gi'
 import { SiWebmoney } from 'react-icons/si'
 
 const Profile = () => {
   const { isLoggedIn, user } = useAuth()
+  const [profileData, setProfileData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${user.token}`,
+        },
+      };
+      try {
+        const response = await axios.get('https://woolee-backend-riosumit.vercel.app/api/profile', config);
+        console.log(response.data)
+        setProfileData(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className='flex flex-col gap-4 font-poppins'>
       <div className='bg-zinc-900 pt-20 px-10 pb-4'>
@@ -18,7 +40,7 @@ const Profile = () => {
             <GiTakeMyMoney size={30} />
           </div>
           <div className="text-right">
-            <p className="text-2xl">24</p>
+            <p className="text-2xl">{profileData?.forsale_count}</p>
             <p>For Sale</p>
           </div>
         </div>
@@ -45,17 +67,17 @@ const Profile = () => {
             <SiWebmoney size={30} />
           </div>
           <div className="text-right">
-            <p className="text-2xl">67</p>
+            <p className="text-2xl">{profileData?.inventory_count}</p>
             <p>In Inventory</p>
           </div>
         </div>
         <div className="bg-zinc-800 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-zinc-600 dark:border-gray-600 text-white font-medium group">
           <div className="flex justify-center items-center w-14 h-14 bg-white text-zinc-800 rounded-full transition-all duration-300 transform group-hover:rotate-12">
-            <GiPayMoney size={30} />
+            <GiSheep size={30} />
           </div>
           <div className="text-right">
-            <p className="text-2xl">$23,780</p>
-            <p>Asset</p>
+            <p className="text-2xl">{profileData?.details?.sheep_count}</p>
+            <p>Sheep count</p>
           </div>
         </div>
       </div>
@@ -70,7 +92,7 @@ const Profile = () => {
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
                   Username
                 </label>
-                <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="lucky.jesse" />
+                <input disabled type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.user?.first_name} />
               </div>
             </div>
             <div className="w-full lg:w-6/12 px-4">
@@ -78,23 +100,23 @@ const Profile = () => {
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
                   Email address
                 </label>
-                <input type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="jesse@example.com" />
+                <input disabled type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.user?.username} />
               </div>
             </div>
             <div className="w-full lg:w-6/12 px-4">
               <div className="relative w-full mb-3">
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  First Name
+                  Phone
                 </label>
-                <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Lucky" />
+                <input disabled type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.phone} />
               </div>
             </div>
             <div className="w-full lg:w-6/12 px-4">
               <div className="relative w-full mb-3">
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  Last Name
+                  Farm Name
                 </label>
-                <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Jesse" />
+                <input disabled type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.farm_name} />
               </div>
             </div>
           </div>
@@ -105,52 +127,28 @@ const Profile = () => {
             Contact Information
           </h6>
           <div className="flex flex-wrap">
-            <div className="w-full lg:w-12/12 px-4">
+            <div className="w-full lg:w-4/12 px-4">
               <div className="relative w-full mb-3">
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  Address
+                  District
                 </label>
-                <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" />
+                <input disabled type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.district} />
               </div>
             </div>
             <div className="w-full lg:w-4/12 px-4">
               <div className="relative w-full mb-3">
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  City
+                  Pincode
                 </label>
-                <input type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="New York" />
+                <input disabled type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.pincode} />
               </div>
             </div>
             <div className="w-full lg:w-4/12 px-4">
               <div className="relative w-full mb-3">
                 <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  Country
+                  State
                 </label>
-                <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="United States" />
-              </div>
-            </div>
-            <div className="w-full lg:w-4/12 px-4">
-              <div className="relative w-full mb-3">
-                <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  Postal Code
-                </label>
-                <input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value="Postal Code" />
-              </div>
-            </div>
-          </div>
-
-          <hr className="mt-6 border-b-1 border-blueGray-300" />
-
-          <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-            About Me
-          </h6>
-          <div className="flex flex-wrap">
-            <div className="w-full lg:w-12/12 px-4">
-              <div className="relative w-full mb-3">
-                <label className="block uppercase text-zinc-800 text-xs font-bold mb-2" htmlfor="grid-password">
-                  About me
-                </label>
-                <textarea type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"> A beautiful UI Kit and Admin for JavaScript &amp; Tailwind CSS. It is Freeand Open Source.</textarea>
+                <input disabled type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-zinc-800 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={profileData?.details?.state} />
               </div>
             </div>
           </div>
